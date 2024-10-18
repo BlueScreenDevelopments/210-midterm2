d5f01900314584e072b9cd08ae2e9bad59e8a67f
Brad Julao <pogiboybrad@gmail.com>
2024-10-17 20:09:22 -0700

    10 Minute Commit

--- 210-midterm2.cpp
+++ 210-midterm2.cpp
@@ -13,151 +13,71 @@
 
 using namespace std;
 
-const int MIN_NR = 10, MAX_NR = 99, MIN_LS = 5, MAX_LS = 20;
-class DoublyLinkedList {
+//Node Structure for the DoublyLinkedList
+struct Node {
+	string name;
+	Node* prev;
+	Node* next;
+	Node(string val) : name(val), prev(nullptr), next(nullptr) {}
+};
+
+//DoublyLinkedList class to manage the line of customers
+class DoublyLinkedList{
 private:
-	struct Node {
-		int data;
-		Node* prev;
-		Node* next;
-		Node(int val, Node* p = nullptr, Node* n = nullptr) {
-			data = val;
-			prev = p;
-			next = n;
-		}
-	};
 	Node* head;
 	Node* tail;
+
 public:
-	DoublyLinkedList() { head = nullptr; tail = nullptr; }
-	void insert_after(int value, int position) {
-		if (position < 0) {
-			cout << "Position must be >= 0." << endl;
-			return;
-		}
-		Node* newNode = new Node(value);
-		if (!head) {
+	DoublyLinkedList() { head = nullptr; tail = nullptr; {}
+
+	//Add a customer to the end of the line.
+	void push_back(const string & name) {
+		Node* newNode = new Node(name);
+		if (!tail) {
 			head = tail = newNode;
-			return;
 		}
-		Node* temp = head;
-		for (int i = 0; i < position && temp; ++i)
-			temp = temp->next;
-		if (!temp) {
-			cout << "Position exceeds list size. Node not inserted.\n";
-			delete newNode;
-			return;
-		}
-		newNode->next = temp->next;
-		newNode->prev = temp;
-		if (temp->next)
-			temp->next->prev = newNode;
-		else
-			tail = newNode;
-		temp->next = newNode;
-	}
-	void delete_val(int value) {
-		if (!head) return;
-		Node* temp = head;
-		while (temp && temp->data != value)
-			temp = temp->next;
-		if (!temp) return;
-		if (temp->prev)
-			temp->prev->next = temp->next;
-		else
-			head = temp->next;
-		if (temp->next)
-			temp->next->prev = temp->prev;
-		else
-			tail = temp->prev;
-		delete temp;
-	}
-	void delete_pos(int pos) {
-		if (!head) {
-			cout << "List is empty." << endl;
-			return;
-		}
-		if (pos == 1) {
-			pop_front();
-			return;
-		}
-		Node* temp = head;
-		for (int i = 1; i < pos; i++) {
-			if (!temp) {
-				cout << "Position doesn't exist." << endl;
-				return;
-			}
-			else
-				temp = temp->next;
-		}
-		if (!temp) {
-			cout << "Position doesn't exist." << endl;
-			return;
-		}
-		if (!temp->next) {
-			pop_back();
-			return;
-		}
-		Node* tempPrev = temp->prev;
-		tempPrev->next = temp->next;
-		temp->next->prev = tempPrev;
-		delete temp;
-	}
-	void push_back(int v) {
-		Node* newNode = new Node(v);
-		if (!tail)
-			head = tail = newNode;
 		else {
 			tail->next = newNode;
 			newNode->prev = tail;
 			tail = newNode;
 		}
+		cout << name << " joined the line." << endl;
 	}
-	void push_front(int v) {
-		Node* newNode = new Node(v);
-		if (!head)
-			head = tail = newNode;
-		else {
-			newNode->next = head;
-			head->prev = newNode;
-			head = newNode;
-		}
-	}
-	void pop_front() {
-		if (!head) {
-			cout << "List is empty." << endl;
-			return;
-		}
+
+	//Serve the customer at the front of the line
+	void serve_customer() {
+		if (!head) return;
+		cout << head->name << " is served." << endl;
 		Node* temp = head;
-		if (head->next) {
-			head = head->next;
-			head->prev = nullptr;
-		}
-		else
-			head = tail = nullptr;
+		head = head->next;
+		if (head) head->prev = nullptr;
+		else tail = nullptr; // Line is now empty
 		delete temp;
 	}
-	void pop_back() {
-		if (!tail) {
-			cout << "List is empty." << endl;
-			return;
-		}
+
+	// Remove the last customer (frustrated)
+	void remove_last() {
+		if (!tail) return;
+		cout << tail->name << " (at the rear) left the line." << endl;
 		Node* temp = tail;
-		if (tail->prev) {
-			tail = tail->prev;
-			tail->next = nullptr;
-		}
-		else
-			head = tail = nullptr;
+		tail = tail->prev;
+		if (tail) tail->next = nullptr;
+		else head = nullptr; // Line is now empty
 		delete temp;
 	}
-	~DoublyLinkedList() {
-		while (head) {
-			Node* temp = head;
-			head = head->next;
-			delete temp;
-		}
+
+	//Line Check (Empty?)
+	bool is_empty() {
+		return head == nullptr;
 	}
+
+	//Print to the user the current line
+
+	//									//
+	// Old Code Below, Delete as Needed //
+	//									//
+	
+	
 	void print() {
 		Node* current = head;
 		if (!current) {
